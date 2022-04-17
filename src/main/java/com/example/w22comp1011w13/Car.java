0c578ef40a20b80a65ffdae86e9a5e4d3ac5ddd4
Jiaqi Chen <200477892@student.georgianc.on.ca>
2022-04-16 22:36:24 -0400

    type sort functionality added.

--- src/main/java/com/example/w22comp1011w13/Car.java
+++ src/main/java/com/example/w22comp1011w13/Car.java
@@ -39,6 +39,11 @@ public class Car {
     }
 
     public String toString() {
-        return String.format("%d-%s %s %.2f", id, make, model, price);
+        return String.format("%d-%s %s %s %.2f", id, types,make, model, price);
+    }
+
+    //check specific type exists.
+    public boolean isType(String typeToCheck) {
+        return types.contains(typeToCheck);
     }
 }
--- src/main/java/com/example/w22comp1011w13/DealerViewController.java
+++ src/main/java/com/example/w22comp1011w13/DealerViewController.java
@@ -1,5 +1,7 @@
 package com.example.w22comp1011w13;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -9,6 +11,7 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.TreeSet;
 
 public class DealerViewController implements Initializable {
 
@@ -30,5 +33,35 @@ public class DealerViewController implements Initializable {
         Dealership dealership = ReadJson.getDealershipFromJson("carData.json");
         dealerNameLabel.setText(dealership.getDealershipName());
         carListView.getItems().addAll(dealership.getInventory());
+        inventory = dealership.getInventory();
+
+        TreeSet<String> types = new TreeSet<>();
+        for(Car car : inventory) {
+            for (String type: car.getTypes()) {
+                types.add(type);
+            }
+        }
+        typeofCarListView.getItems().addAll(types);
+
+        //attach a listener to the type list view to filter the list of cars.
+        typeofCarListView.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, typeSelected)->{
+            //clear existing filter/selection
+        carListView.getItems().clear();
+
+        //loop over the inventory to check if the car is the correct type
+//            if it is add to the list
+        for (Car car: inventory) {
+            if (car.isType(typeSelected)) {
+                carListView.getItems().add(car);
+            }
+        }
+//            System.out.println("Type selected " + newValue);
+        });
     }
+    //What type of data structure will sort elements and prevent duplicates?
+    //tree - sorting.
+    //sets - prevent duplicates.
+    //treeset - both. comparable..
+    //hashset - use hash value to sort
+    //map ?
 }
